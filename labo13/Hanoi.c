2f24e9aed77051458d56c1d40196003cf14455bc
YounesJulien <younes.julien@courrier.uqam.ca>
2020-04-14 18:29:51 -0400

    Ajout fichier labo13

--- labo13/Hanoi.c
+++ labo13/Hanoi.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 
-void deplacer (int n, char t1, char t2, char t3)
-{
-if ( n == 1 )
-printf("De la tour %c à la tour %c\n", t1, t2) ;
-else
-{ deplacer (n-1, t1, t3, t2) ;
-deplacer ( 1 , t1, t2, t3) ;
-deplacer (n-1, t3, t2, t1) ;
-}
+void deplacer (int n, char t1, char t2, char t3) {
+
+	if ( n == 0) {
+		return;
+	}
+
+	if ( n == 1 ) {
+		printf("%c à %c\n", t1, t2) ;
+	}
+		
+	else {
+		deplacer (n-1, t1, t3, t2) ;
+		deplacer ( 1 , t1, t2, t3) ;
+		deplacer (n-1, t3, t2, t1) ;
+	}
 }
 
 int main () {
-	int n = 3;
-	deplacer (n, 'A', 'C', 'B');
+
+	for (int i = 0; i <= 3; i++){
+		printf("\nAppel %d: deplacerTour(%d, A, C, B)\n",i+1,i);
+		deplacer (i, 'A', 'C', 'B');
+	}
 	return 0;
 }
 
